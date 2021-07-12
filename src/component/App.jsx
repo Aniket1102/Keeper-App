3f729ed85ba70149637dbb33816955b7ac16630a
Aniket Bhavsar <aniket.bhavsar1102@gmail.com>
2021-07-12 08:46:16 -0600

    created files and design the website

---
+++ src/component/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer";
+import Heading from "./Heading";
+import Notes from "./Notes";
+
+function App (){
+
+    return (
+        <div>
+    <Heading/>
+    <Notes/>
+    <Footer/>
+    </div>
+    );
+}
+export default App;
---
+++ src/component/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes(){
+    return(
+        <div className="note">
+            <h1>
+This is note title
+            </h1>
+            <p>
+This is note description
+            </p>
+        </div>
+    );
+}
+
+export default Notes;
